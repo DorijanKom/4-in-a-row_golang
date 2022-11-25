bfe7f7a8f205f246f714a1b1358d4e84dc95eb90
DorijanKom <dorijan.komsic@stu.ibu.edu.ba>
2022-11-25 18:43:44 +0100

    Board generation func started

---
+++ go.mod
@@ -0,0 +1,3 @@
+module burch/4-in-a-row
+
+go 1.19
--- main.go
+++ main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("This is an SSH commit test")
+	var row, col int
+
+	fmt.Println("Please input the number of rows for your game.")
+	fmt.Scan(&row)
+
+	fmt.Println("Please enter the number of columns for your game.")
+	fmt.Scan(&col)
+
+	board := generateBoard(row, col)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			board[i][j] = "d"
+			fmt.Printf("| %s ", board[i][j])
+		}
+		fmt.Printf("|")
+		fmt.Println()
+	}
+
+}
+
+func generateBoard(row, col int) [][]string {
+	board := make([][]string, row)
+
+	for i := range board {
+		board[i] = make([]string, col)
+	}
+
+	return board
 }
